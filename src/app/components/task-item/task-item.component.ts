99c79b06d24616cea892e2195e042a54419276f8
Ryan Ross <ryan@reross.com>
2022-06-22 11:54:51 -0500

    Added double click function for tasks to toggle reminder status.
    Created add-task component.

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import {Task} from "../../Task";
 import {faTimes} from '@fortawesome/free-solid-svg-icons'
 @Component({
@@ -7,11 +7,24 @@ import {faTimes} from '@fortawesome/free-solid-svg-icons'
   styleUrls: ['./task-item.component.css']
 })
 export class TaskItemComponent implements OnInit {
-@Input() task!: Task
+
+  @Input() task!: Task
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter()
   faTimes = faTimes;
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onDelete = (task: Task) => {
+    this.onDeleteTask.emit(task);
+  }
+
+  onToggle = (task: Task) => {
+    this.onToggleReminder.emit(task);
   }
 
 }
